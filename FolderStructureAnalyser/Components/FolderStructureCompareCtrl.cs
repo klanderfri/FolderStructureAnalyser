443c4a2c282606ffa74c1f09072389a97c64b316
klanderfri <registrering+github@bjornlarsson.se>
2020-02-17 16:53:57 +0100

    Implemented folder selection.

--- FolderStructureAnalyser/Components/FolderStructureCompareCtrl.cs
+++ FolderStructureAnalyser/Components/FolderStructureCompareCtrl.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using FolderStructureAnalyser.GUI;
 using FolderStructureAnalyser.SessionBound;
 
 namespace FolderStructureAnalyser.Components
@@ -29,7 +24,59 @@ namespace FolderStructureAnalyser.Components
 
         public void CompareFolderStructures()
         {
-            throw new NotImplementedException();
+            if (mayStartCompare())
+            {
+                var paths = askUserForFoldersToCompare();
+                CompareFolderStructures(paths);
+            }
+        }
+
+        private void CompareFolderStructures(Tuple<string, string> paths)
+        {
+            if (mayStartCompare(paths))
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private Tuple<string, string> askUserForFoldersToCompare()
+        {
+            var result = xtraFolderBrowserDialogOriginalFolder.ShowDialog();
+            var originalFolder = (result == DialogResult.OK) ? xtraFolderBrowserDialogOriginalFolder.SelectedPath : null;
+
+            xtraFolderBrowserDialogCloneFolder.ShowDialog();
+            var clonedFolder = (result == DialogResult.OK) ? xtraFolderBrowserDialogCloneFolder.SelectedPath : null;
+
+            return new Tuple<string, string>(originalFolder, clonedFolder);
+        }
+
+        private bool mayStartCompare()
+        {
+            if (backgroundWorkerCompareFolders.IsBusy)
+            {
+                MessageBoxes.ShowAnalyseInProgressMessage();
+                return false;
+            }
+            return true;
+        }
+
+        private bool mayStartCompare(Tuple<string, string> paths)
+        {
+            if (!mayStartCompare()) { return false; }
+            if (!pathIsValid(paths.Item1)) { return false; }
+            if (!pathIsValid(paths.Item2)) { return false; }
+            return true;
+        }
+
+        private bool pathIsValid(string path)
+        {
+            if (String.IsNullOrWhiteSpace(path)) { return false; }
+            if (!Directory.Exists(path))
+            {
+                MessageBoxes.ShowDirectoryDoesNotExistMessage(path);
+                return false;
+            }
+            return true;
         }
     }
 }
