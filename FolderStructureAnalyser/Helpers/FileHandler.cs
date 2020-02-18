b1b556fde745aa85addd2c33b3a7ac3a8a67f274
klanderfri <registrering+github@bjornlarsson.se>
2020-02-18 17:01:00 +0100

    Created method handling directories as well and added bug fixes.

--- FolderStructureAnalyser/Helpers/FileHandler.cs
+++ FolderStructureAnalyser/Helpers/FileHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace FolderStructureAnalyser.Helpers
@@ -15,15 +16,45 @@ namespace FolderStructureAnalyser.Helpers
         /// <returns>A dictionary containing the file names and the files themselves.</returns>
         public static Dictionary<string, FileInfo> GetFileIndex(string folderPath)
         {
-            var folder = new DirectoryInfo(folderPath);
-            var files = new Dictionary<string, FileInfo>();
+            Func<DirectoryInfo, IEnumerable<FileSystemInfo>> getSubItems = delegate (DirectoryInfo folder) { return folder.GetFiles(); };
+            return getDiskItemIndex<FileInfo>(folderPath, getSubItems);
+        }
 
-            foreach (var file in folder.GetFiles())
+        /// <summary>
+        /// Gives an index of the subfolders within a folder.
+        /// </summary>
+        /// <param name="folderPath">The full path to the folder containing the subfolders.</param>
+        /// <returns>A dictionary containing the folder names and the folders themselves.</returns>
+        public static Dictionary<string, DirectoryInfo> GetDirectoryIndex(string folderPath)
+        {
+            Func<DirectoryInfo, IEnumerable<FileSystemInfo>> getSubItems = delegate (DirectoryInfo folder) { return folder.GetDirectories(); };
+            return getDiskItemIndex<DirectoryInfo>(folderPath, getSubItems);
+        }
+
+
+        private static Dictionary<string, T> getDiskItemIndex<T>(string folderPath, Func<DirectoryInfo, IEnumerable<FileSystemInfo>> getSubItems) where T : FileSystemInfo
+        {
+            var folder = new DirectoryInfo(folderPath);
+            var subItems = new Dictionary<string, T>();
+
+            try
             {
-                files.Add(file.Name, file);
+                foreach (var subItem in getSubItems(folder))
+                {
+                    if ((subItem as T) == null)
+                    {
+                        throw new NullReferenceException("The subitem cannot be null");
+                    }
+
+                    subItems.Add(subItem.Name, subItem as T);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                //Oops! Well, just skip disk items we don't have access to.
             }
 
-            return files;
+            return subItems;
         }
     }
 }
