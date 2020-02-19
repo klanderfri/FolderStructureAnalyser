5a7c50ef85e92b0dbadf47b035c98d8a28eedbff
klanderfri <registrering+github@bjornlarsson.se>
2020-02-19 14:12:04 +0100

    Added method fetching the hit info from a grid, and put into a helper class.

---
+++ FolderStructureAnalyser/Helpers/GridHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Base.ViewInfo;
+
+namespace FolderStructureAnalyser.Helpers
+{
+    /// <summary>
+    /// Class for handling grid components.
+    /// </summary>
+    public static class GridHandler
+    {
+        /// <summary>
+        /// Gets the hit info for a certain position.
+        /// </summary>
+        /// <param name="grid">The grid holding the hit info.</param>
+        /// <param name="screenHitLocation">The location, in screen coordinates, to get the hit info for.</param>
+        /// <returns>The hit info for the specified position.</returns>
+        public static BaseHitInfo GetHitInfo(GridControl grid, Point screenHitLocation)
+        {
+            var gridHitLocation = grid.PointToClient(screenHitLocation);
+            var view = grid.GetViewAt(gridHitLocation);
+            return view.CalcHitInfo(gridHitLocation);
+        }
+    }
+}
