a0f5d8ba7e87f87e6ae0312f0b8d7bc83db30e79
klanderfri <registrering+github@bjornlarsson.se>
2020-02-21 20:42:12 +0100

    Added human readable description of log message type enum.

--- FolderStructureAnalyser/Enums/LogMessageType.cs
+++ FolderStructureAnalyser/Enums/LogMessageType.cs
@@ -1,4 +1,7 @@
-﻿namespace FolderStructureAnalyser.Enums
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FolderStructureAnalyser.Enums
 {
     /// <summary>
     /// Tells which kind a log message is.
@@ -6,13 +9,17 @@
     public enum LogMessageType
     {
         /// <summary>
-        /// A message about how long the operation took.
+        /// Log message telling how long time an operation took.
         /// </summary>
+        [Display(Name = "Operation time")]
+        [Description("Log message telling how long time an operation took.")]
         OperationTime,
 
         /// <summary>
         /// Any other log message.
         /// </summary>
+        [Display(Name = "Miscellaneous")]
+        [Description("Any other log message.")]
         Miscellaneous
     }
 }
