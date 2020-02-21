8e484e93f22ab127b75f7c829b284daa43ebb8e7
klanderfri <registrering+github@bjornlarsson.se>
2020-02-21 21:33:41 +0100

    Made the operation time update method update the last operation time log without removing older ones.

--- FolderStructureAnalyser/Components/UserPanels/OperationMessageLogCtrl.cs
+++ FolderStructureAnalyser/Components/UserPanels/OperationMessageLogCtrl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Forms;
 using FolderStructureAnalyser.DataObjects;
 using FolderStructureAnalyser.Enums;
@@ -15,6 +16,11 @@ namespace FolderStructureAnalyser.Components.UserPanels
         /// The ID that will be assigned the next log message.
         /// </summary>
         private int NextLogID { get; set; }
+
+        /// <summary>
+        /// Gives the last log message in the grid.
+        /// </summary>
+        private LogMessage LastLog { get; set; }
 
         /// <summary>
         /// The data source holding the log messages to show in the grid.
@@ -45,7 +51,13 @@ namespace FolderStructureAnalyser.Components.UserPanels
             var format = "Last operation time: {0} sec";
             var message = String.Format(format, runtimeInSeconds);
 
-            //Add the log message.
+            //Remove the previous time log of the operation.
+            if (LastLog?.Type == LogMessageType.OperationTime)
+            {
+                removeLogMessage(LastLog.ID);
+            }
+
+            //Add the new operation time log.
             addLogMessage(LogMessageType.OperationTime, message);
         }
 
@@ -63,8 +75,10 @@ namespace FolderStructureAnalyser.Components.UserPanels
         /// </summary>
         /// <param name="type">The type of the log message.</param>
         /// <param name="message">The human readable log message.</param>
-        private void addLogMessage(LogMessageType type, string message)
+        /// <returns>The ID of the added log message.</returns>
+        private int addLogMessage(LogMessageType type, string message)
         {
+            //Create the log message.
             var log = new LogMessage()
             {
                 ID = NextLogID++,
@@ -72,7 +86,42 @@ namespace FolderStructureAnalyser.Components.UserPanels
                 Type = type,
                 Message = message
             };
+            
+            //Add the log message to the grid.
             LogMessages.Add(log);
+            LastLog = log;
+
+            //Return the ID of the added log message.
+            return log.ID;
+        }
+
+        /// <summary>
+        /// Removes a log message from the grid.
+        /// </summary>
+        /// <param name="logID">The ID of the log message to remove.</param>
+        /// <returns>TRUE if the log message was found and removed, else FALSE.</returns>
+        private bool removeLogMessage(int logID)
+        {
+            //The log can not be in the grid if its ID hasn't been used yet.
+            if (logID >= NextLogID) { return false; }
+
+            //Try finding the old log.
+            var oldLog = LogMessages.FirstOrDefault(l => l.ID == logID);
+
+            //Check if the old log was found.
+            if (oldLog == null) { return false; }
+
+            //Remove the old log.
+            LogMessages.Remove(oldLog);
+
+            //Update the property holding the last log message.
+            if (LastLog.ID == oldLog.ID)
+            {
+                LastLog = LogMessages.LastOrDefault();
+            }
+
+            //Return that the old log was found and removed.
+            return true;
         }
     }
 }
