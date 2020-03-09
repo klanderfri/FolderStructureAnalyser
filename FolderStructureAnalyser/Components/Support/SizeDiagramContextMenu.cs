b76b495fbf5359260bef390918a6266e349680c0
klanderfri <registrering+github@bjornlarsson.se>
2020-03-09 17:24:49 +0100

    Removed unused usings.

--- FolderStructureAnalyser/Components/Support/SizeDiagramContextMenu.cs
+++ FolderStructureAnalyser/Components/Support/SizeDiagramContextMenu.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
-using DevExpress.Utils.Svg;
 using DevExpress.XtraTreeMap;
 using FolderStructureAnalyser.Components.AnalyserPanels;
 using FolderStructureAnalyser.DataObjects;
