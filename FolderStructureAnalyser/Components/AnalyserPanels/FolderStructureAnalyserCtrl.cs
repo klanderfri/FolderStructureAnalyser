c20f6c801b224d07711a43a965cc11ffc7afa5f0
klanderfri <registrering+github@bjornlarsson.se>
2020-03-09 17:52:19 +0100

    Removing unused using.

--- FolderStructureAnalyser/Components/AnalyserPanels/FolderStructureAnalyserCtrl.cs
+++ FolderStructureAnalyser/Components/AnalyserPanels/FolderStructureAnalyserCtrl.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using DevExpress.XtraEditors.Controls;
 using DevExpress.XtraTreeList;
